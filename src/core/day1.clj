299c25734ea3fde38bdf43f28cefeb74fa6862f4
Kostas Georgiadis <kongeor@gmail.com>
2024-12-01 22:39:05 +0200

    aocooo

--- src/core/day1.clj
+++ src/core/day1.clj
@@ -1,6 +1,10 @@
 (ns core.day1
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.core.logic :refer :all]
+            [clojure.core.logic.unifier :as u]
+            [clojure.core.logic.fd :as fd])
+  (:refer-clojure :exclude [==]))
 
 
 
@@ -48,3 +52,66 @@
   (day1b (parse-input (slurp (io/resource "input1.txt"))))
   ;; => 20719933
   )
+
+(comment
+  (run* [q]
+    (membero q [1 2 3])
+    (membero q [2 3 4])))
+
+(comment
+  (run 5 [q]
+    (fd/in q (fd/interval 1 10))
+    (fd/> q 1)))
+
+(defne inserto
+  [x l1 l2]
+  ([_ () [x]])
+  ([_ [y . t1] [x y . t1]]
+   (fd/>= y x))
+  ([_ [y . t1] [y . t2]]
+   (fd/< y x)
+   (inserto x t1 t2)))
+
+(comment
+  (run 2 [q]
+    (inserto 3 [1 2] q)))
+
+(comment
+  (run 2 [q]
+    (inserto 3 q [1 2 3 4])))
+
+(defne sorto
+  [l1 l2]
+  #_([() ()])                                               ;; will yield one extra result
+  ([[x] [x]])
+  ([[x . t1] _]
+   (fresh [tt]
+     (sorto t1 tt)
+     (inserto x tt l2))))
+
+(comment
+  (run 2 [q]
+    (sorto [10 5 2 3 1 7] q))
+  ;; this doesn't work
+  #_(run 3 [q]
+    (sorto q [10 5 2 3 1 7])))
+
+#_(run 1 [q]
+    (== {:a q} {:a 1}))
+
+(defne freqio
+  [l f]
+  ([[x] [[x 1]]])
+  ([[x . t] [[x n]]]
+   (fresh [n' f']
+     (fd/+ n' 1 n)
+     (== f' [[x n']])
+     (freqio t f')))
+  ([[x . t] [[y _] . t3]]
+   (fresh [f']
+     (conso [x 1] f' f)
+     (freqio t f'))))
+
+(comment
+  (run 3 [q]
+    (freqio [1 1 2 3 3 3 4 4] q)))
